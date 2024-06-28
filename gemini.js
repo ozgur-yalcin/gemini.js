6db751ab6174d1ebe6f429eb0efdeb78cccf0478
Özgür Yalçın <ozguryalcin@outlook.com>
2024-06-28 04:52:30 +0300

    Update gemini.js

--- gemini.js
+++ gemini.js
@@ -1,11 +1,11 @@
 import * as fs from 'node:fs/promises';
 import * as readline from 'node:readline/promises';
-import { stdin as input, stdout as output } from 'node:process';
+import { stdin, stdout } from 'node:process';
 import { GoogleGenerativeAI } from "@google/generative-ai";
 (async () => {
     const key = await fs.readFile("api.key");
     const data = await fs.readFile("data.txt");
-    const rl = readline.createInterface({ input, output });
+    const rl = readline.createInterface({ input: stdin, output: stdout });
     const ai = new GoogleGenerativeAI(key.toString().trim());
     const model = ai.getGenerativeModel({ model: "gemini-1.5-flash" });
     const prompt = await rl.question("prompt> ");
