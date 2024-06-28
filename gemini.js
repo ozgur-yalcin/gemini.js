e8c4e4d73cb51dca9e83543b3d30cc26d5ef66c3
Özgür Yalçın <ozguryalcin@outlook.com>
2024-06-28 04:49:20 +0300

    Update gemini.js

--- gemini.js
+++ gemini.js
@@ -4,15 +4,11 @@ import { stdin as input, stdout as output } from 'node:process';
 import { GoogleGenerativeAI } from "@google/generative-ai";
 (async () => {
     const key = await fs.readFile("api.key");
+    const data = await fs.readFile("data.txt");
     const rl = readline.createInterface({ input, output });
     const ai = new GoogleGenerativeAI(key.toString().trim());
     const model = ai.getGenerativeModel({ model: "gemini-1.5-flash" });
-    const chat = model.startChat();
-    while (true) {
-        const prompt = await rl.question("prompt:");
-        if (prompt === "exit") break;
-        const content = await chat.sendMessage(prompt);
-        console.log(content.response.text().trim());
-    }
-    rl.close();
+    const prompt = await rl.question("prompt> ");
+    const content = await model.generateContent([prompt, data.toString()]);
+    console.log(content.response.text().trim());
 })();
